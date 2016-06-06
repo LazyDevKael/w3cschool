e4222273845a223e63d74b79c1d14714b5b6cfd4
System Administrator <root@max.local>
2016-06-06 19:02:35 +1000

    Chaper 4 finished

---
+++ Javascript/Ouput/myScript.js
@@ -0,0 +1,21 @@
+
+function writeToAlertBox() {
+    window.alert("Hello, world.")
+}
+
+function writeToHTMLOutput() {
+    document.write("Hello, world.")
+}
+
+function writeToHTMLElement() {
+    var demo = document.getElementById("demo")
+    demo.innerHTML = "Hello, world."
+}
+
+function writeToBrowserConsole() {
+    console.log("Hello, world.")
+}
+
+
+
+

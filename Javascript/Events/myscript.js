98a55519688b521154e92ba8bc438d3cd458b78b
LazyDevKael <DevKael@gmail.com>
2016-06-10 20:53:42 +1000

    Events section is finished

---
+++ Javascript/Events/myscript.js
@@ -0,0 +1,14 @@
+
+function displayDateTime() {
+	// Javascript change the content of html page component "demo" to 
+	// the current date time
+	// The component doesn't need to be a button
+	document.getElementById("demo").innerHTML = Date()
+}
+
+function alterDateTime() {
+	window.alert(Date())	
+}
+
+
+

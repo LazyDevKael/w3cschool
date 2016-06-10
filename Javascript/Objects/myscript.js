5aeced6880bc095da71494fc9ee20edcc31d2a81
LazyDevKael <devkael@gmail.com>
2016-06-10 19:47:16 +1000

    Object section is finished

--- Javascript/Objects/myscript.js
+++ Javascript/Objects/myscript.js
@@ -6,3 +6,24 @@ function displayFirstName() {
 
     document.getElementById("demo").innerHTML = person.firstName
 }
+
+function displayCountry() {
+	var person = {
+		firstName: "Kael",
+		lastName: "Xu",
+		country: "USA"
+	}
+
+	document.getElementById("demo").innerHTML = person["country"]
+}
+
+function displayInfo() {
+	var person = {
+		name: "Kael",
+		age: 22
+	}
+
+	document.getElementById("demo").innerHTML = person.name + " is " + person.age + " years old"
+}
+
+
